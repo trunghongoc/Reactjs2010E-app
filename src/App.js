cc80c7b82ed3c5cae5c53f242cf47bce5ada6f19
Trung Ho Ngoc - macpro 13 2018 <trung0194@gmai.com>
2021-01-10 18:47:41 +0700

    Add sample code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './scss/index.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Hoc reactjs</h1>
+        <h2>Count: {this.state.count}</h2>
+
+        <button onClick={this.increment}>Increment 1</button>
+{/*
+        <button onClick={() => this.increment(1)}>Increment 1</button>
+        <button onClick={() => this.increment(2)}>Increment 2</button> */}
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
